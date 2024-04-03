f7e667123fe9c7ecb3d727d61a4b694898281eaa
Alp <alps-examples@mailbox.org>
2024-04-03 18:56:42 +0200

    Extend containers

--- containers/containers.cpp
+++ containers/containers.cpp
@@ -28,3 +28,26 @@ TEST(Containers, VectorOddErase)
     EXPECT_EQ(v.size(), 1);
 }
 
+
+TEST(Containers, VectorInit)
+{
+    std::vector<int> v1 (2);
+    EXPECT_EQ(v1.size(), 2);
+    EXPECT_EQ(v1[0], 0);
+    EXPECT_EQ(v1[1], 0);
+
+    std::vector<int> v2 (2, -1);
+    EXPECT_EQ(v2.size(), 2);
+    EXPECT_EQ(v2[0], -1);
+    EXPECT_EQ(v2[1], -1);
+
+    std::vector<int> v3 = { -1, -2 };
+    EXPECT_EQ(v3.size(), 2);
+    EXPECT_EQ(v3[0], -1);
+    EXPECT_EQ(v3[1], -2);
+
+    std::vector<std::pair<size_t, size_t>> v4 = { {-1,-1}, {-2, -2} };
+    for(auto [a,b] : v4) {
+        EXPECT_EQ(a,b);
+    }
+}
